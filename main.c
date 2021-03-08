689d6407dde493335245fe77a0d2086448254bd6
jacksonns <42253628+jacksonns@users.noreply.github.com>
2021-03-08 18:41:10 -0300

    colocando tempo e tabela não formatada

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
-#include <locale.h>
 
 #include "memory.h"
 
 int main(int argc, char *argv[]){
+	
 	if(argc < 4){
 		return 0;
 	}
-	setlocale(LC_ALL,"");
+	
 	printf("Iniciando execução...\n");
 	printf("Arquivo de entrada: %s\n", argv[2]);
 	printf("Tamanho da memória: %s KB\n",argv[4] );
@@ -14,21 +14,24 @@ int main(int argc, char *argv[]){
 	printf("Método de substituição: %s\n", argv[1]);
 
 	Memory* mem = CreateMemory(atoi(argv[3]), atoi(argv[4]));
-	printf("Memória criada\n");
+	
+	double time = 0.0;
+	clock_t begin = clock();
 	RunMemory(mem,argv[2],argv[1]);
+	clock_t end = clock();
+	time += (double)(end - begin) / CLOCKS_PER_SEC;
 	
 	printf("Total de acessos: %d\n", mem->num_data);
-	printf("Frames alocados: %d\n",mem->occupied_frames_num);
-	printf("Valor de s: %d\n", mem->s);
 	printf("Page faults: %lld\n", mem->pageFaults);
 	printf("Dirty pages: %lld\n", mem->dirtyBits);
-	printf("Memoria lidas: %lld\n", mem->readBits);
+	printf("Memoria lida: %lld\n", mem->readBits);
 	printf("Memoria escrita: %lld\n",mem->writtenBits);
+	printf("Tempo de execucao: %f segundos\n",time);
 	
-	
-	printf("Páginas alocadas:\n");
+	printf("Tabela de páginas final:\n");
+	printf("\tÍndice\tPágina\tdirty bit\n");
 	for (int i = 0; i < mem->max_frames_num; i++){
-		printf("Página %d\n", mem->p_frames[i].virtual_id);
+		printf("\t%d\t%d\t%d\n", i, mem->p_frames[i].virtual_id, mem->p_frames[i].written_);
 	}
 	
 	
